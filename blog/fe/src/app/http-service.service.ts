99626d6cd335765c753d9b49f4945d63eaed03e2
Kamil Janeček <j@necek.com>
2020-01-05 14:10:10 +0100

    tmp

--- blog/fe/src/app/http-service.service.ts
+++ blog/fe/src/app/http-service.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from './../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  private baseUrl = 'http://localhost:5000';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
